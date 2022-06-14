6811aac35d317d9037dab47b50012bbde3ddba70
mikakallberg <mikaela.kallberg@hotmail.com>
2022-06-14 15:37:56 +0000

    Add tasks function

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,11 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // sidenav initialization
+    // Sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
-});
 
-
-document.addEventListener('DOMContentLoaded', function() {
+    //Modal initialization
     let modal = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(modal);
+    M.Modal.init(modal);
+
+    //Datapicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: 'dd mmm, yyyy',
+        i18n: {done: "Select"}
     });
+
+    //Select initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+
+    //Collapsible initialization
+    let collapsible = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsible);
+});
